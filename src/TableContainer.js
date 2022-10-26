f9069d16dba871b9abf2b2747fac33ad725b665c
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2022-10-25 20:30:14 -0600

    Add files via upload

---
+++ src/TableContainer.js
@@ -0,0 +1,74 @@
+import React, { Fragment } from 'react';
+import { useTable, useSortBy, useFilters, useExpanded } from 'react-table';
+import { Table } from 'reactstrap';
+import { Filter, DefaultColumnFilter } from './filters';
+
+const TableContainer = ({ columns, data, renderRowSubComponent }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    visibleColumns,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn: { Filter: DefaultColumnFilter },
+    },
+    useFilters,
+    useSortBy,
+    useExpanded
+  );
+
+  const generateSortingIndicator = (column) => {
+    return column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : '';
+  };
+
+  return (
+    <Table bordered hover {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>
+                <div {...column.getSortByToggleProps()}>
+                  {column.render('Header')}
+                  {generateSortingIndicator(column)}
+                </div>
+                <Filter column={column} />
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <Fragment key={row.getRowProps().key}>
+              <tr>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+              {row.isExpanded && (
+                <tr>
+                  <td colSpan={visibleColumns.length}>
+                    {renderRowSubComponent(row)}
+                  </td>
+                </tr>
+              )}
+            </Fragment>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableContainer;
